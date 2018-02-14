5bb45b08a82966c954e93a4fd6bba95d7fbe938e
johannalundb <32523835+johannalundb@users.noreply.github.com>
2018-02-14 13:37:18 +0100

    new update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
+	// Show default image on startpage timeout.
+	var timeout;
+
 	$(".rubrik").hover(function () {
+		clearTimeout(timeout);
+
 		$("body").addClass($(this).data("body-class"));
+		$("#default-image").removeClass("visible-image");
 	}, function () {
 		$("body").removeClass($(this).data("body-class"));
+
+		timeout = setTimeout(function () {
+			$("#default-image").addClass("visible-image");
+		}, 900);
 	});
+
+
 });
